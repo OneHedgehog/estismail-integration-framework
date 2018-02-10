34bc173cc60b4103415ffb51facad6cfb7289488
OneHedgehog <aperteda1@gmail.com>
2018-02-10 20:12:43 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,4 @@
 <?php
-
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
 //require autoload ( care with namespaces )
 require_once __DIR__ . '/vendor/autoload.php';
 
